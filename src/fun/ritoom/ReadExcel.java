48bba1b185b1b6f471b56b2afd3660d095b4b45d
Ritoom <queans@mail.com>
2019-06-18 11:29:13 +0800

    程序间联动

--- src/fun/ritoom/ReadExcel.java
+++ src/fun/ritoom/ReadExcel.java
@@ -7,45 +7,77 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class ReadExcel {
     public static void main(String[] args) throws IOException {
-//        XSSFWorkbook workbook = new XSSFWorkbook();
-//        FileOutputStream outputStream = new FileOutputStream(new File("file.xlsx"));
-//        workbook.write(outputStream);
-//        outputStream.close();
-//        System.out.println("创建成功");
         readExcelFile();
     }
 
     static XSSFRow row;
+    static ArrayList<TableCommand> stringsColumn = new ArrayList<>();
+    static String table_name = "rf_caigxjgxx";
     public static void readExcelFile() throws IOException {
-        FileInputStream fileInputStream = new FileInputStream(new File("file.xlsx"));
+        FileInputStream fileInputStream = new FileInputStream(new File("file_new.xlsx"));
         XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
         XSSFSheet sheet = workbook.getSheetAt(0);
         Iterator<Row> rowIterator = sheet.iterator();
 
-        while (rowIterator.hasNext()){
-            row = (XSSFRow)rowIterator.next();
-            Iterator <Cell>  cellIterator = row.cellIterator();
-
-            while (cellIterator.hasNext()){
-                Cell cell = cellIterator.next();
-
-                switch (cell.getCellType()){
-                    case Cell.CELL_TYPE_NUMERIC:
-                        System.out.print(cell.getNumericCellValue() + " \t\t ");
-                        break;
-
-                    case Cell.CELL_TYPE_STRING:
-                        System.out.print(
-                                cell.getStringCellValue() + " \t\t ");
-                        break;
+        for(int i=sheet.getFirstRowNum();i<sheet.getLastRowNum();i++){
+            TableCommand tableCommand= new TableCommand();
+            Row row = sheet.getRow(i);
+            for (int j=row.getFirstCellNum();j<=row.getLastCellNum();j++){
+                Cell cell = row.getCell(j);
+                if (j == 0){
+                    tableCommand.setColumn_name(cell.getStringCellValue());
+                }
+                if (j == 1){
+                    tableCommand.setNote(cell.getStringCellValue());
+                }
+                if (j == 2){
+                    tableCommand.setCommand(cell.getStringCellValue());
                 }
             }
-            System.out.println();
+            stringsColumn.add(tableCommand);
+        }
+
+        for (TableCommand str :
+                stringsColumn) {
+            System.out.println(str.toString());
+            if (str.getColumn_name().length() > 0 && str.getColumn_name() != null){
+                ImportMysql.importMySql(str,table_name);
+            }
         }
         fileInputStream.close();
+//        while (rowIterator.hasNext()){
+//            row = (XSSFRow)rowIterator.next();
+//            Iterator <Cell>  cellIterator = row.cellIterator();
+//            String s = null;
+//
+//            TableCommand tableCommand = new TableCommand();
+//            while (cellIterator.hasNext()){
+//                Cell cell = cellIterator.next();
+//
+//                switch (cell.getCellType()){
+//                    case Cell.CELL_TYPE_STRING:
+//                        s = cell.getStringCellValue();
+//                        stringsColumn.add(s);
+//                        break;
+//                }
+//                //每列
+//                System.out.println(stringsColumn);
+//            }
+//            //每行
+//            //System.out.println(strings.get(strings.size() -1).toString());
+//            //String[] culumn = s.split(" ");
+////            tableCommand.setTable_name(strings);
+////            tableCommand.setColumn_name(culumn[1]);
+////            tableCommand.setCommand(culumn[2]);
+////            System.out.println(tableCommand.toString());
+//
+//            //ImportMysql.importMySql(tableCommand);
+        //}
+        //fileInputStream.close();
     }
 }
